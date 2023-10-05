53608e0cf1b459e4f01011fe6944859051418faa
shawn edgell <shawn_edgell@hotmail.com>
2023-10-05 16:23:53 -0400

    overhauled entire app, functions like a note app

--- app.js
+++ app.js
@@ -1,144 +1,114 @@
-const myLibrary = [
-  new Book('Book 1', 'Author 1', 200, true),
-  new Book('Book 2', 'Author 2', 300, false),
-  new Book('Book 3', 'Author 3', 300, false),
-];
+const titleInput = document.getElementById('titleInput');
+const authorInput = document.getElementById('authorInput');
+const pagesInput = document.getElementById('pagesInput');
+const bookForm = document.getElementById('bookForm');
+const bookList = document.getElementById('bookList');
 
-function Book(title, author, pages, hasRead) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.hasRead = hasRead;
+initApp();
 
-  this.info = function() {
-      return `${this.title} by ${this.author}, ${this.pages} pages, ${this.hasRead ? 'already read' : 'not read yet'}`;
-  };
-
-  this.toggleReadStatus = function() {
-      this.hasRead = !this.hasRead;
-  };
+function initApp() {
+    populateBooksFromStorage();
+    bookForm.addEventListener('submit', handleAddBook);
 }
 
-function addBookToLibrary(title, author, pages, hasRead) {
-  const hasReadValue = hasRead === 'yes' ? true : false;
-  const newBook = new Book(title, author, pages, hasReadValue);
-  myLibrary.push(newBook);
-  displayBooks();
-  document.getElementById('new-book-button').style.display = 'block';
-  
-  showNotification("Book added successfully!");
+function populateBooksFromStorage() {
+    getStoredBooks().reverse().forEach(book => addBookToList(book));
 }
 
-function displayBooks() {
-  const bookList = document.getElementById('book-list');
-  bookList.innerHTML = ''; // Clear the previous book list
+function handleAddBook(event) {
+    event.preventDefault();
 
-  myLibrary.forEach((book, index) => {
-      const bookCard = createBookCard(book, index);
-      bookList.appendChild(bookCard);
-  });
+    const book = {
+        title: titleInput.value.trim(),
+        author: authorInput.value.trim(),
+        pages: pagesInput.value.trim()
+    };
+
+    if (book.title && book.author && book.pages) {
+        addBookToList(book);
+        prependToStoredBooks(book);
+
+        clearInputs();
+    }
 }
 
-function createBookCard(book, index) {
-  const bookCard = document.createElement('div');
-  bookCard.classList.add('book-card');
-
-  const bookDetails = document.createElement('ul');
-  const detailsArray = [
-    { label: "Title", value: book.title },
-    { label: "Author", value: book.author },
-    { label: "Pages", value: book.pages },
-    { label: "Read Yet", value: book.hasRead ? 'Yes' : 'No' }
-  ];
-
-  detailsArray.forEach(detail => {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `<strong>${detail.label}:</strong> ${detail.value}`;
-    bookDetails.appendChild(listItem);
-  });
-
-  const toggleButton = createButton('Toggle Read Status', index, () => toggleReadStatus(index));
-  const removeButton = createButton('Remove', index, () => removeBook(index));
-
-  bookCard.appendChild(bookDetails);
-  bookCard.appendChild(toggleButton);
-  bookCard.appendChild(removeButton);
-
-  return bookCard;
+function clearInputs() {
+    titleInput.value = '';
+    authorInput.value = '';
+    pagesInput.value = '';
 }
 
-function createButton(text, index, clickHandler) {
-  const button = document.createElement('button');
-  button.textContent = text;
-  button.setAttribute('data-index', index);
-  button.addEventListener('click', clickHandler);
-  return button;
+function addBookToList(book) {
+    const li = createBookElement(book);
+    bookList.insertBefore(li, bookList.children[1]);
 }
 
-function toggleReadStatus(index) {
-  myLibrary[index].toggleReadStatus();
-  displayBooks();
+function createBookElement(book) {
+    const li = document.createElement('li');
+    li.style.wordBreak = "break-all";
+    
+    li.appendChild(createDetailsDiv(book));
+    li.appendChild(createDeleteButton());
+
+    return li;
 }
 
-function removeBook(index) {
-  myLibrary.splice(index, 1);
-  displayBooks();
-  
-  showNotification("Book removed successfully!");
+function createDetailsDiv(book) {
+    const detailsDiv = document.createElement('div'); 
+    detailsDiv.style.flexGrow = '1';
+    detailsDiv.style.width = '80%';
+
+    detailsDiv.appendChild(createDetail("Title:", book.title));
+    detailsDiv.appendChild(createDetail("Author:", book.author));
+    detailsDiv.appendChild(createDetail("Pages:", book.pages));
+
+    return detailsDiv;
 }
 
-document.getElementById('new-book-button').addEventListener('click', () => openBookForm());
+function createDetail(labelText, detailText) {
+    const div = document.createElement('div');
+    const label = document.createElement('strong');
+    label.textContent = labelText;
+    div.appendChild(label);
+    div.appendChild(document.createTextNode(` ${detailText}`));
 
-document.querySelector('.close-form-button').addEventListener('click', () => closeBookForm());
-
-document.getElementById('add-book-form').addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  const pages = document.getElementById('pages').value;
-  const hasReadDropdown = document.getElementById('has-read');
-  const hasRead = hasReadDropdown.options[hasReadDropdown.selectedIndex].value;
-
-  addBookToLibrary(title, author, pages, hasRead);
-  document.getElementById('add-book-form').reset();
-  closeBookForm();
-});
-
-function openBookForm() {
-  const bookForm = document.getElementById('book-form');
-  bookForm.style.display = 'block';
-  document.querySelector('.close-form-button').style.display = 'block';
-  document.getElementById('new-book-button').style.display = 'none';
+    return div;
 }
 
-function closeBookForm() {
-  const bookForm = document.getElementById('book-form');
-  bookForm.style.display = 'none';
-  document.querySelector('.close-form-button').addEventListener('click', () => closeBookForm());
-  document.getElementById('new-book-button').style.display = 'block';
-  
+function createDeleteButton() {
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'X';
+    deleteButton.className = 'delete';
+    deleteButton.addEventListener('click', handleDeleteBook);
+    return deleteButton;
 }
 
-displayBooks();
-
-document.getElementById('dark-mode-toggle').addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
-});
-
-function showNotification(message) {
-  const notification = document.getElementById('notification');
-  notification.textContent = message;
-  notification.style.opacity = '1';
-  notification.style.visibility = 'visible';
-
-  setTimeout(() => {
-      hideNotification();
-  }, 3000); // Hide notification after 3 seconds
+function handleDeleteBook(event) {
+    bookList.removeChild(event.target.parentElement);
+    updateStoredBooks();
 }
 
-function hideNotification() {
-  const notification = document.getElementById('notification');
-  notification.style.opacity = '0';
-  notification.style.visibility = 'hidden';
+function getStoredBooks() {
+    return JSON.parse(localStorage.getItem('books') || '[]');
+}
+
+function prependToStoredBooks(book) {
+    const books = getStoredBooks();
+    books.unshift(book);
+    localStorage.setItem('books', JSON.stringify(books));
+}
+
+function updateStoredBooks() {
+    const books = Array.from(bookList.children)
+                       .slice(1)
+                       .map(li => ({
+                           title: extractDetailFromDiv(li, 1),
+                           author: extractDetailFromDiv(li, 2),
+                           pages: extractDetailFromDiv(li, 3)
+                       }));
+    localStorage.setItem('books', JSON.stringify(books));
+}
+
+function extractDetailFromDiv(li, childIndex) {
+    return li.querySelector(`div:nth-child(${childIndex})`).textContent.split(': ')[1].trim();
 }
